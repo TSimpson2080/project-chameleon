a1b633fe48d99691be888c61a204cee58caf4594
Tom Simpson <tom@depositcoin.xyz>
2026-01-21 14:06:22 -0500

    Fix ZipWriter LE append helpers (remove invalid append)

--- Chameleon/Sources/Services/Export/ZipWriter.swift
+++ Chameleon/Sources/Services/Export/ZipWriter.swift
@@ -128,15 +128,15 @@ private enum CRC32 {
 private extension Data {
     mutating func appendUInt16LE(_ value: UInt16) {
         var v = value.littleEndian
-        Swift.withUnsafeBytes(of: &v) { buffer in
-            append(contentsOf: buffer.bindMemory(to: UInt8.self))
+        Swift.withUnsafeBytes(of: &v) { raw in
+            append(contentsOf: raw)
         }
     }
 
     mutating func appendUInt32LE(_ value: UInt32) {
         var v = value.littleEndian
-        Swift.withUnsafeBytes(of: &v) { buffer in
-            append(contentsOf: buffer.bindMemory(to: UInt8.self))
+        Swift.withUnsafeBytes(of: &v) { raw in
+            append(contentsOf: raw)
         }
     }
 }
